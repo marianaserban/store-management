8553e19870bee341dce6f434d0af49f78606b3e6
marianaserban <marianaserban734@gmail.com>
2025-07-10 19:55:15 +0300

    Project setup

---
+++ store-management-tool/src/main/java/store/management/tool/StoreManagementToolApplication.java
@@ -0,0 +1,13 @@
+package store.management.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementToolApplication.class, args);
+	}
+
+}
---
+++ store-management-tool/src/test/java/store/management/tool/StoreManagementToolApplicationTests.java
@@ -0,0 +1,13 @@
+package store.management.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementToolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
