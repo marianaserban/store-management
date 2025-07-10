377b08d6b0fe424511c3488b165efdf2ec54b70e
marianaserban <marianaserban734@gmail.com>
2025-07-10 20:24:50 +0300

    Create product entity

---
+++ store-management-tool/src/main/java/store/management/tool/entity/Product.java
@@ -0,0 +1,33 @@
+package store.management.tool.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name",nullable = false)
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    @Positive(message = "Price must be greater than 0!")
+    private double price;
+
+    @Column(name = "quantity")
+    @Min(value = 0, message = "Quantity must be greater than or equal to 0!")
+    private int quantity;
+}
